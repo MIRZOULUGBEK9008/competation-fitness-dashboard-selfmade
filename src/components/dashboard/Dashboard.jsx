ba6472accdab299e004f3998e1da0acd3ea0aaa0
Ibrohimjon  Muhammadjonov <115447690+Ibrohimqor1@users.noreply.github.com>
2023-09-24 18:36:47 +0300

    Changes the style of dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,43 +1,49 @@
 import React from 'react'
 import "./Dashboard.css"
-
+import MainPhoto from "../../assets/icons/icon-main-min.svg"
+import Logo from "../../assets/gantel.png"
 const Dashboard = () => {
   return (
     <div>
         <div className='sidebar_overall'>
         <div className='sidebar container'>
         <div className='sidebar-parts'>
-                <img src="" alt="" />
+                <img src={Logo} alt="" />
 
                  
             </div>
             <div className='sidebar-parts'>
-                <img src="" alt="" />
+                <img src={MainPhoto} alt="" />
 
                  <h5>Asosiy</h5>
             </div>
 
             <div className='sidebar-parts'>
-
+            <img src={MainPhoto} alt="" />
                  <h5>Mijozlar</h5>
             </div>
 
 
             <div className='sidebar-parts'>
-
-                 <h5>Asosiy</h5>
+            <img src={MainPhoto} alt="" />
+                 <h5>Juhozlar</h5>
             </div>
 
 
             <div className='sidebar-parts'>
-
-                 <h5>Asosiy</h5>
+            <img src={MainPhoto} alt="" />
+                 <h5>Maxsulotlar</h5>
             </div>
 
             <div className='sidebar-parts'>
-
-                 <h5>Asosiy</h5>
+            <img src={MainPhoto} alt="" />
+                 <h5>Savol javob</h5>
             </div>
+            <div className='sidebar-parts'>
+            <img src={MainPhoto} alt="" />
+                 <h5>Statistika</h5>
+            </div>
+
 
 
 
