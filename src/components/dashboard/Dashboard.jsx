08a59d443ca257d652e013def372c93831ec9493
Mirzo Ulug'bek Xudoyberdiyev <ulugbekkhudoyberdiyev9008@gmail.com>
2023-09-24 21:09:41 +0500

    Add Dashboard.jsx

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,61 +1,41 @@
-import React from 'react'
-import "./Dashboard.css"
-import MainPhoto from "../../assets/icons/icon-main-min.svg"
-import Logo from "../../assets/gantel.png"
+import React from "react";
+import "./Dashboard.css";
+
 const Dashboard = () => {
   return (
     <div>
-        <div className='sidebar_overall'>
-        <div className='sidebar container'>
-        <div className='sidebar-parts'>
-                <img src={Logo} alt="" />
+      <div className="sidebar_overall">
+        <div className="sidebar container">
+          <div className="sidebar-parts">
+            <img src="" alt="" />
+          </div>
+          <div className="sidebar-parts">
+            <img src="" alt="" />
 
-                 
-            </div>
-            <div className='sidebar-parts'>
-                <img src={MainPhoto} alt="" />
+            <h5>Asosiy</h5>
+          </div>
 
-                 <h5>Asosiy</h5>
-            </div>
+          <div className="sidebar-parts">
+            <h5>Mijozlar</h5>
+          </div>
 
-            <div className='sidebar-parts'>
-            <img src={MainPhoto} alt="" />
-                 <h5>Mijozlar</h5>
-            </div>
+          <div className="sidebar-parts">
+            <h5>Asosiy</h5>
+          </div>
 
+          <div className="sidebar-parts">
+            <h5>Asosiy</h5>
+          </div>
 
-            <div className='sidebar-parts'>
-            <img src={MainPhoto} alt="" />
-                 <h5>Juhozlar</h5>
-            </div>
-
-
-            <div className='sidebar-parts'>
-            <img src={MainPhoto} alt="" />
-                 <h5>Maxsulotlar</h5>
-            </div>
-
-            <div className='sidebar-parts'>
-            <img src={MainPhoto} alt="" />
-                 <h5>Savol javob</h5>
-            </div>
-            <div className='sidebar-parts'>
-            <img src={MainPhoto} alt="" />
-                 <h5>Statistika</h5>
-            </div>
-
-
-
-
+          <div className="sidebar-parts">
+            <h5>Asosiy</h5>
+          </div>
         </div>
 
-        <div className='sidebar-asosiy container'>
-            menu
-
-        </div>
-        </div>
+        <div className="sidebar-asosiy container">menu</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
