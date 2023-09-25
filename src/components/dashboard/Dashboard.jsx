1841d6ed1564d0a6564a73d76f815b092422156e
Mirzo Ulug'bek Xudoyberdiyev <ulugbekkhudoyberdiyev9008@gmail.com>
2023-09-25 18:56:10 +0500

    Add Ibrohimjon's changes

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,11 +4,49 @@ import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
+<<<<<<< Updated upstream
     <div className="container">
     
        <h1>Asosiy</h1>
        
       
+=======
+    <div>
+      <div className="sidebar container">
+        <div className="sidebar-parts">{/* <img src="" alt="" /> */}</div>
+        <div className="sidebar-parts">
+          <img src={MainPhoto} alt="" />
+
+          <h5>Asosiy</h5>
+        </div>
+
+        <NavLink to="/Mijozlar">
+          <div className="sidebar-parts">
+            <img src={MainPhoto} alt="" />
+            <h5>Mijozlar</h5>
+          </div>
+        </NavLink>
+
+        <div className="sidebar-parts">
+          <img src={MainPhoto} alt="" />
+          <h5>Jihozlar</h5>
+        </div>
+
+        <div className="sidebar-parts">
+          <img src={MainPhoto} alt="" />
+          <h5>Maxsulotlar</h5>
+        </div>
+
+        <div className="sidebar-parts">
+          <img src={MainPhoto} alt="" />
+          <h5>Savol javob</h5>
+        </div>
+        <div className="sidebar-parts">
+          <img src={MainPhoto} alt="" />
+          <h5>Statistika</h5>
+        </div>
+      </div>
+>>>>>>> Stashed changes
     </div>
   );
 };
